95d66b5b0e2d45cb215c3db572ee5efaa5521e2f
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2023-12-15 20:26:11 +0200

    dzddzdzd--modal

---
+++ index1.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var openModalBtn = document.getElementById('openModalBtn');
+    var modal = document.getElementById('myModal');
+    var closeModalSpan = document.getElementsByClassName('close')[0];
+
+
+    openModalBtn.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+
+    closeModalSpan.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
